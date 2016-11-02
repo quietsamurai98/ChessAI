74187707ded4a815bbd63a554850012035bd60ed
quietsamurai98 <quietsamurai98@gmail.com>
2016-11-02 13:45:35 -0500

    Add files via upload

---
+++ MainRunner.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)MainRunner.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/11/2
+ */
+
+public class MainRunner {
+        
+    /**
+     * Creates a new instance of <code>MainRunner</code>.
+     */
+    public MainRunner() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ChessGUI gui=new ChessGUI(false,true);
+    }
+}
