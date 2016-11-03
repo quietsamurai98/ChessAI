c9930a9a251636b5a595367bd76530b9a579b9b1
quietsamurai98 <quietsamurai98@gmail.com>
2016-11-03 10:11:47 -0500

    Add files via upload

--- MainRunner.java
+++ MainRunner.java
@@ -5,7 +5,11 @@
  * @author 
  * @version 1.00 2016/11/2
  */
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
 public class MainRunner {
         
     /**
@@ -18,6 +22,44 @@ public class MainRunner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ChessGUI gui=new ChessGUI(false,true);
+    	JFrame newGameFrame = new JFrame();
+    	newGameFrame.setTitle("Chess Interface");
+        
+        newGameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        newGameFrame.setFocusable(true);
+        newGameFrame.requestFocus();
+    	newGameFrame.setLayout(new GridLayout(2,2,0,0));
+    	JButton humanVhuman = new JButton("Human vs Human");
+    	humanVhuman.addMouseListener(new java.awt.event.MouseAdapter() {
+        	public void mouseClicked(java.awt.event.MouseEvent e) {
+        		ChessGUI gui=new ChessGUI(false,false);
+        	}          
+      	});
+    	JButton aiVai = new JButton("AI vs AI");
+    	aiVai.addMouseListener(new java.awt.event.MouseAdapter() {
+        	public void mouseClicked(java.awt.event.MouseEvent e) {
+        		ChessGUI gui=new ChessGUI(true,true);
+        	}          
+      	});
+    	JButton humanVai = new JButton("Human vs AI \r(Play as White)");
+    	humanVai.addMouseListener(new java.awt.event.MouseAdapter() {
+        	public void mouseClicked(java.awt.event.MouseEvent e) {
+        		ChessGUI gui=new ChessGUI(false,true);
+        	}          
+      	});
+    	JButton aiVhuman = new JButton("Human vs AI \r(Play as Black)");
+    	aiVhuman.addMouseListener(new java.awt.event.MouseAdapter() {
+        	public void mouseClicked(java.awt.event.MouseEvent e) {
+        		ChessGUI gui=new ChessGUI(true,false);
+        	}          
+      	});
+    	newGameFrame.add(humanVhuman);
+    	newGameFrame.add(aiVai);
+    	newGameFrame.add(humanVai);
+    	newGameFrame.add(aiVhuman);
+    	newGameFrame.pack();
+        newGameFrame.setResizable(false);
+        newGameFrame.setVisible(true);
+        
     }
 }
